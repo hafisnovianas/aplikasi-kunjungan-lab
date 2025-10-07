21d10d6eb3da2a2bc08383539e853201d2d7ed70
Hafis Novianas <novianashafis@gmail.com>
2025-10-07 23:09:11 +0700

    refactor home

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -4,17 +4,21 @@ const HomePage = {
   async render() {
     return `
       <div id="home" class="page-view">
-        <div class="card">
-          <div class="promo-card__content">
-            <div class="promo-card__text">
-              <h2>OPEN RECRUITMENT ASISTEN 2025</h2>
-              <p>Jadilah bagian dari tim Laboratorium Fisika UMRI. Tunjukkan kemampuan dan dedikasimu!</p>
+        <div id="infoList">
+          <!--
+          <div class="card">
+            <div class="promo-card__content">
+              <div class="promo-card__text">
+                <h2>OPEN RECRUITMENT ASISTEN 2025</h2>
+                <p>Jadilah bagian dari tim Laboratorium Fisika UMRI. Tunjukkan kemampuan dan dedikasimu!</p>
+              </div>
+              <div class="promo-card__image">
+                <img src="src/public/asisten2.png">
+              </div>
             </div>
-            <div class="promo-card__image">
-              <img src="src/public/asisten2.png">
-            </div>
+            <a href="https://docs.google.com/forms/d/1_PSJH5d1gB_UmVtXuZ4PunvOIDW8ERrb_z4FCgtYQZM" class="btn-primary">Daftar Sekarang!</a>
           </div>
-          <a href="https://docs.google.com/forms/d/1_PSJH5d1gB_UmVtXuZ4PunvOIDW8ERrb_z4FCgtYQZM" class="btn-primary">Daftar Sekarang!</a>
+          -->
         </div>
 
         <div class="card">
@@ -29,83 +33,93 @@ const HomePage = {
           <ul id="dailyVisitList" class="visit-card__list"></ul>
         </figure>
 
-        <a class="add-visit-button a-icon" href="#/visit"><i class="fa-solid fa-plus"></i>Kunjungan</a>
+        <a class="add-visit-button a-icon" href="#/visit">
+          <i style="margin-right: 0.5rem; font-weight: 900;" class="fa-solid fa-plus"></i>Kunjungan
+        </a>
       </div>
     `
   },
 
-  // 
-
   async afterRender() {
-    loadLeaderboard();
-    loadDailyHistory();
-  }
-};
+    try {
+      await Promise.all([
+        this._loadLeaderboard(),
+        this._loadDailyHistory()
+      ]);
+    } catch (error) {
+      console.error("Gagal memuat semua data:", error);
+    }
+  },
 
-export default HomePage;
-
-async function loadDailyHistory() {
-    const todaysHistoryContent = document.getElementById('dailyVisitList');
+  async _loadDailyHistory() {
     const totalVisitElement = document.getElementById('totalVisit')
+    const todaysHistoryContent = document.getElementById('dailyVisitList');
     todaysHistoryContent.innerHTML = '<p class="text-center">Memuat...</p>';
 
     try {
-        const response = await CallApi.callApi('getDailyHistory');
-        const dataLength = response.data.length;
-        totalVisitElement.textContent = dataLength;
-        if (response.status === 'success') {
-            todaysHistoryContent.innerHTML = '';
-            if (dataLength === 0) {
-                todaysHistoryContent.innerHTML = '<p class="text-center">Belum ada kunjungan hari ini.</p>';
-            } else {
-                response.data.forEach(visit => {
-                    const item = document.createElement('li');
-                    item.className = 'visit-list-item';
-                    item.innerHTML = `
-                      <div class="visit-list-item__main">
-                        <h6 class="visit-list-item__name">${visit.name}</h6>
-                        <p>${visit.purpose}</p>
-                      </div>
-                      <span class="visit-list-item__time">${visit.time}</span>
-                    `;
-                    todaysHistoryContent.appendChild(item);
-                });
-            }
-        } else {
-            throw new Error(response.message);
-        }
-    } catch (error) {
-        todaysHistoryContent.innerHTML = `<p class="text-center text-danger">Gagal memuat riwayat hari ini.</p>`;
-    }
-}
+      const response = await CallApi.callApi('getDailyHistory');
+      if (response.status !== 'success') throw new Error(response.message);
 
-async function loadLeaderboard() {
+      const { data } = response;
+      totalVisitElement.textContent = data.length;
+      todaysHistoryContent.innerHTML = '';
+      if (data.length === 0) {
+        todaysHistoryContent.innerHTML = '<p class="text-center">Belum ada kunjungan hari ini.</p>';
+        return
+      }
+
+      data.forEach(visit => {
+        const item = document.createElement('li');
+        item.className = 'visit-list-item';
+        item.innerHTML = `
+          <div class="visit-list-item__main">
+            <h6 class="visit-list-item__name">${visit.name}</h6>
+            <p>${visit.purpose}</p>
+          </div>
+          <span class="visit-list-item__time">${visit.time}</span>
+        `;
+        todaysHistoryContent.appendChild(item);
+      });
+    } catch (error) {
+      todaysHistoryContent.innerHTML = `<p class="text-center text-danger">Gagal memuat riwayat hari ini.</p>`;
+      throw error;
+    }
+  },
+
+  async _loadLeaderboard() {
     const leaderboardList = document.getElementById('leaderboardList');
     leaderboardList.innerHTML = '<p class="text-center">Memuat peringkat...</p>';
 
     try {
-        const response = await CallApi.callApi('getLeaderboard');
-
-        if (response.status === 'success') {
-            leaderboardList.innerHTML = '';
-            if (response.data.length === 0) {
-                leaderboardList.innerHTML = '<p class="text-center">Belum ada data.</p>';
-            } else {
-                response.data.forEach(user => {
-                    const item = document.createElement('li');
-                    item.className = 'card-list__item';
-                    
-                    item.innerHTML = `
-                        <span>${user.name}</span>
-                        <span class="leaderboard-list__badge">${user.visitCount} Kunjungan</span>
-                    `;
-                    leaderboardList.appendChild(item);
-                });
-            }
+      const response = await CallApi.callApi('getLeaderboard');
+      if (response.status !== 'success') throw new Error(response.message)
+      
+      if (response.status === 'success') {
+        leaderboardList.innerHTML = '';
+        if (response.data.length === 0) {
+          leaderboardList.innerHTML = '<p class="text-center">Belum ada data.</p>';
         } else {
-            throw new Error(response.message);
+          response.data.forEach(user => {
+            const item = document.createElement('li');
+            item.className = 'card-list__item';
+            
+            item.innerHTML = `
+              <span>${user.name}</span>
+              <span class="leaderboard-list__badge">${user.visitCount} Kunjungan</span>
+            `;
+            leaderboardList.appendChild(item);
+          });
         }
+      }
     } catch (error) {
-        leaderboardList.innerHTML = `<p class="text-center text-danger">Gagal memuat peringkat.</p>`;
+      leaderboardList.innerHTML = `<p class="text-center text-danger">Gagal memuat peringkat.</p>`;
     }
-}
+  },
+
+  async _addInfo() {
+    const infoContainer = document.querySelector('#infoList')
+  }
+  
+};
+
+export default HomePage;
